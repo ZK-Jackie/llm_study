2359608b2ae1d500a043361a03c1f89e0a1f1933
Jackie <44011334mm@stu.gdou.edu.cn>
2024-02-11 16:22:41 +0800

    feats: supplement the content of the notes or the assignment;

--- notes/03_openxlab_download.py
+++ notes/03_openxlab_download.py
@@ -1,6 +1,5 @@
-# import openxlab
-# openxlab.login(ak="vzranmjgawe4znm8wl56", sk="qmj6zq9dwxlyanennbvyo2p4wn5le3w1dv4gzavp") #进行登录，输入对应的AK/SK
+import openxlab
+openxlab.login(ak="ak", sk="sk") #进行登录，输入对应的AK/SK
 from openxlab.dataset import get
-# get(dataset_repo='OpenDataLab/MATH', target_path='../pics/03') # 数据集下载
-
-get(dataset_repo='OpenDataLab/XiaChuFang_Recipe_Corpus', target_path='../pics/03') # 数据集下载
+# get(dataset_repo='OpenDataLab/MATH', target_path='../pics/03') # 数学题数据集下载
+get(dataset_repo='OpenDataLab/XiaChuFang_Recipe_Corpus', target_path='../pics/03') # 下厨房菜谱数据集下载
