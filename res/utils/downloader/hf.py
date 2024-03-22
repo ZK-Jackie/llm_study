93a8e1621d2ecf0b0b73b5220cbdf74391decc8d
Jackie <44011334mm@stu.gdou.edu.cn>
2024-03-22 20:12:37 +0800

    feat: modelscope uploader/downloader

--- res/utils/downloader/hf.py
+++ res/utils/downloader/hf.py
@@ -4,4 +4,4 @@ import os
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 # 下载模型
-os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /root/data/model/sentence-transformer')
+os.system('huggingface-cli download --resume-download THUDM/chatglm3-6b --local-dir /root/THUDM/chatglm3-6b')
